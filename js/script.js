cf3b4e5db572a74330e4145af846b93475622692
Matthew Younger <matthewyounger33@gmail.com>
2025-10-18 15:50:06 -0500

    Added the endpoint to .js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 console.log("script.js loaded");
+
+// Add the endpoint variable
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=NRAeuq61KFE60yjhn4ggtvlLjv5fKs9f&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
